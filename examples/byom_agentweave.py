711af0f96530f1326b3a8335ffead6c4e8a3a34e
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-08-19 11:24:58 +0530

    Use isolated BYOM extension in example

--- examples/byom_agentweave.py
+++ examples/byom_agentweave.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import asyncio
 
-from agentweave import BYOMAgentWeave, CallableModelAdapter
+from agentweave_byom import BYOMAgentWeave, CallableModelAdapter
 
 TOOLS = [
     {
