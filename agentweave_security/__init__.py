10fc331809918ee7766c8ab0392863b186c6756e
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-08-25 16:29:00 +0530

    feat: add authorization and stage observability package

---
+++ agentweave_security/__init__.py
@@ -0,0 +1,19 @@
+from .authorization import (
+    AuthorizationDecision,
+    AuthorizationGate,
+    AuthorizationPolicy,
+    AuthorizationDenied,
+    CandidateDrop,
+    StageObservation,
+    StageTelemetry,
+)
+
+__all__ = [
+    "AuthorizationDecision",
+    "AuthorizationGate",
+    "AuthorizationPolicy",
+    "AuthorizationDenied",
+    "CandidateDrop",
+    "StageObservation",
+    "StageTelemetry",
+]
